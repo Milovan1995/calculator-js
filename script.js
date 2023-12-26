c5576f6c9cd4070470d121f57924f9c5849d7e80
Milovan <milovan.antic314@gmail.com>
2023-12-26 22:59:59 +0100

    Refactor code

--- script.js
+++ script.js
@@ -1,90 +1,64 @@
+////////////////////////////////////
 //calc function
 const calculator = () => {
   let num1 = "0";
   let result = 0;
   let operation;
 
-  function add() {
-    result += parseInt(num1);
-  }
-  function subtract() {
-    result -= parseInt(num1);
-  }
-  function divide() {
-    if (num1 == 0) {
-      console.error("can't divide by 0");
-      alert("Nope, can't divide by 0");
-    } else {
-      result /= parseInt(num1);
-    }
-  }
-  function multiply() {
-    result *= parseInt(num1);
-  }
-  function modulo() {
-    if (result === 0) {
-      console.error("Cant calc modulo with 0");
-      alert("Can't calculate modulo with 0");
-    } else {
-      result %= parseInt(num1);
-    }
-  }
-  function operate(chosenOperation) {
+  const add = () => (result += parseInt(num1));
+  const subtract = () => (result -= parseInt(num1));
+  const multiply = () => (result *= parseInt(num1));
+  const divide = () =>
+    num1 == 0
+      ? alert("Error, can't divide with 0")
+      : (result /= parseInt(num1));
+  const modulo = () => {
+    num1 == 0
+      ? alert("Can't calculate modulo with 0")
+      : (result %= parseInt(num1));
+  };
+  const operate = (chosenOperation) => {
     switch (chosenOperation) {
       case "+":
-        add();
-        break;
+        return add();
       case "-":
-        subtract();
-        break;
+        return subtract();
       case "*":
-        multiply();
-        break;
+        return multiply();
       case "/":
-        divide();
-        break;
+        return divide();
       case "%":
-        modulo();
-        break;
-      //in case of no selected operator (beggining):
+        return modulo();
+      //when the first operation hasn't been assigned (at start)
       default:
         result = parseInt(num1);
         break;
     }
-  }
-  function assignNum1(newNum) {
+  };
+  const assignNum1 = (newNum) => {
     num1 = num1 === "0" ? `${newNum}` : `${num1}${newNum}`;
-  }
-  function assignOperation(newOperation) {
+  };
+  const assignOperation = (newOperation) => {
     operate(operation);
     operation = newOperation;
     num1 = "0";
-  }
-  function getResult() {
-    return result;
-  }
-  function getOperation() {
-    return operation;
-  }
-  function getNum() {
-    return num1;
-  }
+  };
+  const getResult = () => result;
+  const getOperation = () => operation;
+  const getNum = () => num1;
   return { assignOperation, getResult, getOperation, getNum, assignNum1 };
 };
-
-function displayButtons(calculatorInstance) {
-  //initializing calculator localy inside displayButtons
+/////////////////////////////////////////////////////////
+//function for initializing a calculator
+const startCalculator = (calculatorInstance) => {
+  //initializing necessary closures and assigning them to local scope
   calc = calculatorInstance();
-  // adding number buttons
-  for (let i = 0; i < 10; i++) {
-    const button = document.createElement("button");
-    button.classList.add("numButton");
-    button.innerHTML = i;
-    button.addEventListener("click", () => {
-      calcDisplay().clickNumber(i);
-    });
-    document.querySelector(".buttons").append(button);
-  }
+  screenMethods = onButtonClicks(calc);
+  displayButtons(screenMethods);
+};
+////////////////////////////////////////////////
+//function for displaying number and operator buttons
+const displayButtons = (calcDisplayInstance) => {
   const operators = {
     add: "+",
     subtract: "-",
@@ -93,7 +67,15 @@ function displayButtons(calculatorInstance) {
     modulo: "%",
     equals: "=",
   };
-  // Adding operator buttons
+  for (let i = 0; i < 10; i++) {
+    const button = document.createElement("button");
+    button.classList.add("numButton");
+    button.innerHTML = i;
+    button.addEventListener("click", () => {
+      calcDisplayInstance.clickNumber(i);
+    });
+    document.querySelector(".buttons").append(button);
+  }
   Object.entries(operators).forEach(([key, operator]) => {
     const button = document.createElement("button");
     button.classList.add("opButton");
@@ -101,37 +83,39 @@ function displayButtons(calculatorInstance) {
     button.innerHTML = operator;
     button.addEventListener("click", () => {
       if (key === "equals") {
-        calcDisplay().clickEquals();
+        calcDisplayInstance.clickEquals();
       } else {
-        calcDisplay().clickOperation(operator);
+        calcDisplayInstance.clickOperation(operator);
       }
     });
     document.querySelector(".buttons").append(button);
   });
   document.querySelector(".lowerScreen").innerHTML = calc.getNum();
-}
-//Calling displayButtons and  passing calculator function as argument to initialize local instance of calculator
-displayButtons(calculator);
+};
+/////////////////////////////////////////////
 //function for the calculator screen
-const calcDisplay = () => {
+const onButtonClicks = (calc) => {
   //selecting elements
   const upperCalcScreen = document.querySelector(".upperScreen");
   const lowerCalcScreen = document.querySelector(".lowerScreen");
 
-  //   handling events
-  function clickNumber(numberInfo) {
+  //Handling possible click events
+  const clickNumber = (numberInfo) => {
     calc.assignNum1(numberInfo);
     lowerCalcScreen.innerHTML = calc.getNum();
-  }
-  function clickOperation(operationInfo) {
+  };
+  const clickOperation = (operationInfo) => {
     calc.assignOperation(operationInfo);
     upperCalcScreen.innerHTML = `${calc.getResult()}${calc.getOperation()}`;
     lowerCalcScreen.innerHTML = `${calc.getNum()}`;
-  }
-  function clickEquals() {
+  };
+  const clickEquals = () => {
     calc.assignOperation(calc.getOperation());
     upperCalcScreen.innerHTML = "";
     lowerCalcScreen.innerHTML = `${calc.getResult()}`;
-  }
+  };
   return { clickEquals, clickNumber, clickOperation };
 };
+/////////////////////////////////////
+// RUN THE CALCULATOR
+startCalculator(calculator);
